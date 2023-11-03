231e94b4b56b31cb1e509f4366efd5d9ce1e1a11
Aaron jiles <sirjiles1230@gmail.com>
2023-11-03 14:18:16 -0500

    just finishing up

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,22 @@ import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  const [featPupId, setFeatPupId] = useState(null)
-function handleClick (){
-// blahblah logic blah blah
-}
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log ("puppyList", puppyList)
+  function handleClick() {
+    // blah blah logic blah blah
+  }
+
   return (
-    <div className='App'>
+    <div className="App">
       {puppies.map((puppy) => {
-          return (<p onClick={()=>{ setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}
+        return (
+          <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
           </p>
-          );
+        );
       })}
-      <p>
-      </p>
     </div>
   );
 }
