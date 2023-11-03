72a7cfb106f4bd15c237aebc2c8ffd9f2105fa28
Aaron jiles <sirjiles1230@gmail.com>
2023-11-03 13:54:51 -0500

    onclick and handleclick

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 
+import { useState  } from 'react'
+import { puppyList } from './data.js'
 import './App.css'
 
 function App() {
-  
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+function handleClick (){
 
+}
+
+  console.log ("puppyList", puppyList)
   return (
-    <>
-    
-    </>
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p onClick={handleClick} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+    </div>
   )
 }
 
