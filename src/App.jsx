403392c0cc584671913fe9cabd2be620e14427f9
Aaron jiles <sirjiles1230@gmail.com>
2023-11-03 22:39:41 -0500

    just need to add css

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,27 @@ import './App.css'
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
   const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
 
   function handleClick() {
-    // blah blah logic blah blah
+    addEventListener('click', clcik)
   }
-
+  
   return (
     <div className="App">
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      {featPupId && <p>{ featPupId }</p>}
       {puppies.map((puppy) => {
         return (
-          <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>
+          <p onClick={(handleClick) => {setFeatPupId(puppy.id)}} key={puppy.id}>
             {puppy.name}
           </p>
         );
